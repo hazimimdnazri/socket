b0095d1612b2de3d17a7450b2ea281403744ca19
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2023-09-02 03:16:32 +0800

    123

--- socket.js
+++ socket.js
@@ -3,36 +3,76 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
+const cors = require('cors');
 const io = require('socket.io')(server, {
     cors: { origin : "*" }
 })
 
+let clients = []
 app.use(express.json());
+app.use(cors())
 
-app.get('/', function(req, res) {
-    res.send('Socket is up and running!')
-})
+// app.post('/alert', (req, res) => {
+//     console.log('Alert received.')
+//     if(req.body.SECRET_KEY == process.env.SECRET_KEY){
+//         switch (req.body.apps) {
+//             case 'recruitment':
+//                 io.sockets.emit("getRecruitmentAlert", req.body.message);
+//                 break;
+        
+//             case 'hr':
+//                 io.sockets.emit("getHrAlert", req.body.message);
+//                 break;
+        
+//             default:
+//                 break;
+//         }
+//         res.send('Alert sent.')
+//     } else {
+//         res.send('ERROR!')
+//     }
+// })
 
-app.post('/alert', function(req, res) {
-    console.log('Alert received.')
+eventsHandler = (req, res) => {
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    };
+
+    res.writeHead(200, headers);
+
+    const clientId = Date.now();
+    const newClient = {
+        id: clientId,
+        res
+    };
+  
+    clients.push(newClient);
+  
+    req.on('close', () => {
+        clients = clients.filter(client => client.id !== clientId);
+    });
+}
+
+sendEventsToAll = (newAlert) => {
+    clients.forEach(client => client.res.write(`data: ${JSON.stringify(newAlert)}\n\n`))
+}
+  
+async function triggerEvent(req, res){
     if(req.body.SECRET_KEY == process.env.SECRET_KEY){
-        switch (req.body.apps) {
-            case 'recruitment':
-                io.sockets.emit("getRecruitmentAlert", req.body.message);
-                break;
-        
-            case 'hr':
-                io.sockets.emit("getHrAlert", req.body.message);
-                break;
-        
-            default:
-                break;
-        }
-        res.send('Alert sent.')
+        const newAlert = req.body;
+        delete newAlert['SECRET_KEY'];
+        res.send('Alert sent!')
+        return sendEventsToAll(newAlert);
     } else {
         res.send('ERROR!')
     }
-})
+}
+
+app.get('/', (req, res) => {res.send('Socket is up and running!')})
+app.get('/events', eventsHandler);
+app.post('/trigger', triggerEvent)
 
 io.on('connection', (socket) => {
     console.log(`Connection ${socket.id} established.`)
