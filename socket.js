6ff3685687d4aebcaa2965adecaffb3dbd5a5f37
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2025-03-23 16:01:02 +0800

    Update 2025.

--- socket.js
+++ socket.js
@@ -5,11 +5,10 @@ import { websocket } from './websocket.js';
 
 const app = express();
 const server = createServer(app);
+const io = websocket.initializeWebSocket(server);
 
 app.use(express.json());
 app.use('/', routes);
-
-const io = websocket.initializeWebSocket(server);
 
 server.listen(process.env.PORT, () => {
     console.log(`Websocket server is running on port ${process.env.PORT}`)
